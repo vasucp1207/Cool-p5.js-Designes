27553321bf3be263d20b7ca2c827f963c7072ae9
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2021-12-02 22:16:01 +0530

    Create cone.js

---
+++ 3D-Shapes/cone.js
@@ -0,0 +1,14 @@
+let detailX;
+
+function setup(){
+    createCanvas(100, 100, WEBGL);
+    detailX = createSlider(3, 24, 3);
+    detailX.position(0, height + 5);
+    detailX.style('width', '100px');
+}
+  
+function draw(){
+    background(200);
+    rotateY(millis() / 1000);
+    cone(20, 50, detailX.value(), 10);
+}
